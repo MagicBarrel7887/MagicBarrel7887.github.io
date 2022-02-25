0c264de5118031cce8367eb7d978456fd34e2df9
nate <costerboy03@outlook.com>
2022-02-25 11:43:31 -0600

    added json

---
+++ Portfolio/portfolio_data.js
@@ -0,0 +1,12 @@
+
+console.log('jQuery Loading Local JSON File')
+
+
+$(document).ready(function () {
+    $.getJSON("test.json", function (data) {
+        console.log(data.name); // Prints: Harry
+        console.log(data.age); // Prints: 14
+    }).fail(function () {
+        console.log("An error has occurred.");
+    });
+});
